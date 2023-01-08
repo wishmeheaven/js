3cb27b5404db419f6c51c6c9cf8fe07c53b803ff
wishmeheaven <wishmeheaven3@gmail.com>
2023-01-08 10:35:14 +0200

    js-1.1

---
+++ 1.1_primitive_data_types.js
@@ -0,0 +1,11 @@
+let a = "hello"
+let b = 2
+let c = {name: "Ran"}
+let d = true
+let e = null
+let f = undefined
+let g = Symbol("g")
+
+for (let i = 0; i < 7; i++) {
+    console.log(typeof arguments[i])
+}
