79220aa72f7c33bb11cbdf8764204fbf7673d3dd
wishmeheaven <wishmeheaven3@gmail.com>
2023-01-11 21:18:00 +0200

    8.3(4) A breakthrough : Learn to read, Boy.

--- 8.3_book_utilities.js
+++ 8.3_book_utilities.js
@@ -1,34 +1,43 @@
 const book1 = {
     title: "Nineteen Eighty-Four",
     author: "George Orwell",
-    yearPublished: 1984
-}
+    yearPublished: 2000,
+    lastEdition: null
+
+};
 
 const book2 = {
     title: "The Hitchhiker's Guide to the Galaxy",
     author: "Douglas Adams",
-    yearPublished: 1979
-}
+    yearPublished: 1979,
+    lastEdition: null
+};
 
 const bookUtils = {
-
-    getFirstPublished : function (someBook, someOtherBook) {
-        return someBook.yearPublished === someOtherBook.yearPublished ? [someBook.title, someOtherBook.title] : 
-        someBook.yearPublished > someOtherBook.yearPublished ?
-        someOtherBook.title : someBook.title
-    },
-    lastEdition: ({setNewEdition (someBook, editionYear) {
-            return editionYear >= someBook.yearPublished ?
-                  editionYear : someBook.yearPublished
-    }}),
+    getFirstPublished(someBook, someOtherBook) {
+        if (someBook === someOtherBook) {
+            return "please compare between 2 different books"
+        } else {
+            return someBook.yearPublished === someOtherBook.yearPublished ?
+                `both ${someBook.title} and ${someOtherBook.title} have been published in the same year ${someBook.yearPublished}` :
+                someBook.yearPublished > someOtherBook.yearPublished ?
+                    `${someOtherBook.title} published first (at ${someOtherBook.yearPublished})` :
+                    `${someBook.title} published first (at ${someBook.yearPublished})`
+        }
+    }, 
+    setNewEdition(book, latestEdition) {
+        if (latestEdition >= book.lastEdition) {
+            book.lastEdition = latestEdition
+            return `${book.title} has been updated to ${latestEdition} edition`
+        } else {
+            return `${latestEdition} edition is older than ${book.title}'s ${book.lastEdition} `
+        }
+    }
 }
 
-// function bookToRead(book) {
-//     return `The book ${book.title} was written by ${book.author} in
-//      the year ${book.yearPublished}`
-// }
 
-// console.log(bookToRead(book))
+// + try give both books the same yearPublish
+// + try compare between a book to itself
+console.log(bookUtils.getFirstPublished(book1, book2))
 
-
-
+console.log(bookUtils.setNewEdition(book1, 2001))
