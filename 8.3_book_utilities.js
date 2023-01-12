98a83b5450a4ea833038393c254c594ea03e392e
wishmeheaven <wishmeheaven3@gmail.com>
2023-01-12 02:28:17 +0200

    tough night

--- 8.3_book_utilities.js
+++ 8.3_book_utilities.js
@@ -7,6 +7,10 @@ const book1 = {
     translation: {
         language: [],
         translator: []
+    },
+    publisher: {
+        name: "",
+        location: ""
     }
 };
 
@@ -19,6 +23,10 @@ const book2 = {
     translation: {
         language: [],
         translator: []
+    },
+    publisher: {
+        name: "",
+        location: ""
     }
 };
 
@@ -52,14 +60,25 @@ const bookUtils = {
         }
     },
     setTranslation(book, language, translator) {
-        if (book.translation.language.includes(language) && book.translation.translator.includes(translator)){
-            return `${book.title} has already been translated to ${language} by ${translator}` 
+        if (book.translation.language.includes(language) && book.translation.translator.includes(translator)) {
+            return `${book.title} has already been translated to ${language} by ${translator}`
         } else {
             book.translation.translator.push(translator)
             book.translation.language.push(language)
             return `${book.title} has new translation to ${language} by ${translator}`
         }
-       
+
+    },
+    setPublisher(book, name, location) {
+        book.publisher.name = name
+        book.publisher.location = location
+        return `${book.title} was originally published by ${book.publisher.name} in ${book.publisher.location}`
+    },
+    isSamePublisher(book1, book2) {
+        return Object.is(book1.publisher.name, book2.publisher.name) ?
+            Object.is(book1.publisher.location, book2.publisher.location) ?
+            `Both ${book1.title} and ${book2.title} have been published in ${book1.publisher.location} by ${book1.publisher.name}` : `But not at the same country`
+        `Both ${book1.title} and ${book2.title} have been published in ${book1.publisher.location} by ${book1.publisher.name}` : `But not at the same country`
     }
 }
 
@@ -69,17 +88,22 @@ const bookUtils = {
 
 // + try give both books the same yearPublish
 // + try compare between a book to itself
-// console.log(bookUtils.getFirstPublished(book1, book2))
+console.log(bookUtils.getFirstPublished(book1, book2))
 
-// console.log(bookUtils.setNewEdition(book2, 1979))
-// console.log(bookUtils.setNewEdition(book1, 2020))
-// console.log(bookUtils.setNewEdition(book1, 1955))
+console.log(bookUtils.setNewEdition(book2, 1979))
+console.log(bookUtils.setNewEdition(book1, 2020))
+console.log(bookUtils.setNewEdition(book1, 1955))
 
-// console.log(bookUtils.setNewLanguage(book2, "English"))
-// console.log(bookUtils.setNewLanguage(book2, "German"))
-// console.log(bookUtils.setNewLanguage(book1, "Swedish"))
+console.log(bookUtils.setNewLanguage(book2, "English"))
+console.log(bookUtils.setNewLanguage(book2, "German"))
+console.log(bookUtils.setNewLanguage(book1, "Swedish"))
 
-// console.log(bookUtils.setTranslation(book1, "Hungarian","Hristo Stoychkov")) 
-// console.log(bookUtils.setTranslation(book2, "Italian", "Franchesco Totti")) 
-// console.log(bookUtils.setTranslation(book2, "Italian", "AlexanderDel Piero")) 
-// console.log(bookUtils.setTranslation(book2, "Italian", "Franchesco Totti")) 
+console.log(bookUtils.setTranslation(book1, "Hungarian","Hristo Stoychkov")) 
+console.log(bookUtils.setTranslation(book2, "Italian", "Franchesco Totti")) 
+console.log(bookUtils.setTranslation(book2, "Italian", "AlexanderDel Piero")) 
+console.log(bookUtils.setTranslation(book2, "Italian", "Franchesco Totti")) 
+
+console.log(bookUtils.setPublisher(book1, "Tor Books", "Canada"))
+console.log(bookUtils.setPublisher(book2, "Tor Books", "Canada"))
+
+console.log(bookUtils.isSamePublisher(book1, book2))
