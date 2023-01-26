255a907adc425bccd5873478952663977c2df931
wishmeheaven <wishmeheaven3@gmail.com>
2023-01-26 02:31:31 +0200

    21-4.bulbs_position_polished

--- 21.4_lightbulb/app/21.4.script.js
+++ 21.4_lightbulb/app/21.4.script.js
@@ -2,49 +2,66 @@
 When the user clicks the lightbulb, it should be turned
 on, clicking it again will turn it off*/
 
+
+
+const h1 = document.createElement("h1")
+h1.style.fontFamily = "Poppins, sans-serif"
+h1.style.fontSize = "9rem"
+h1.style.fontWeight = "bold"
+h1.textContent = "Turn on the light"
+h1.style.color = "black"
+h1.style.textShadow = "2px 2px 2px red"
+h1.style.textAlign = "center"
+// h1.style.marginTop = "5rem"
+h1.style.width = "100%"
+document.body.appendChild(h1)
+
 const lightBulb = document.getElementById("lightbulb")
-lightBulb.style.width = "50rem"
-lightBulb.style.height = "50rem"
+lightBulb.style.width = "40rem"
+lightBulb.style.height = "70rem"
+lightBulb.style.margin = "0 auto"
+// lightBulb.style.paddingTop = "9rem"
 
 
 const lightBulbImgOff = document.createElement("img")
 lightBulbImgOff.src = "./assets/images/bulb-off.jpg"
-// lightBulbImgOff.style.width = "30rem"
-// lightBulbImgOff.style.height = "30rem"
-lightBulbImgOff.style.borderRadius = "30px"
-lightBulbImgOff.style.border = "1px solid rgb(255, 212, 132)"
+lightBulbImgOff.style.borderTopLeftRadius = "50%"
+lightBulbImgOff.style.borderTopRightRadius = "50%"
+// lightBulbImgOff.style.paddingBottom = "5rem"
+lightBulbImgOff.style.borderTop = "1px solid rgb(255, 212, 132)"
+lightBulbImgOff.style.width = "110%"
+lightBulbImgOff.style.marginTop = "12rem"
 lightBulb.appendChild(lightBulbImgOff)
 
 const lightBulbImgOn = document.createElement("img")
 lightBulbImgOn.src = "./assets/images/bulb-on.png"
-lightBulbImgOn.style.borderRadius = "30px"
-lightBulbImgOn.style.border = "1px solid  rgb(30, 101, 255)"
+lightBulbImgOn.style.borderTopLeftRadius = "50%"
+lightBulbImgOn.style.borderTopRightRadius = "50%"
+lightBulbImgOn.style.borderTop = "1px solid  rgb(28, 28, 230)"
+lightBulbImgOn.style.boxShadow = "0px 0px 100px 50px rgb(154, 154, 244)"
+lightBulbImgOn.style.paddingRight = "2rem"
 
-
-
-
-// const lightBulbImgOn = "./assets/images/bulb-on.png"
-// const lightBulbImgOff = "./assets/images/bulb-off.jpg"
-
-
-// lightbulb.style.width = "30rem"
-// lightbulb.style.height = "30rem"
-// lightbulb.style.borderRadius = "50%"
-// lightbulb.style.border = "1px solid rgb(242, 236, 199)"
-// lightBulb.style.background = "url(./assets/images/bulb-off.jpg) no-repeat center center"
 
 let isOn = false
 lightBulb.addEventListener("click", () => {
     console.log("lightBulb clicked")
     isOn = !isOn
     if (isOn) {
+        h1.textContent = "Turn the light off"
+        h1.style.textShadow = "15px 20px 20px rgb(154, 154, 244)"
+        h1.style.color = " rgb(18, 0, 255)"    
         lightBulb.appendChild(lightBulbImgOn)
         lightBulb.removeChild(lightBulbImgOff)
-        lightBulbImgOn.style.width = "30rem"
-        lightBulbImgOn.style.height = "40rem"
+        lightBulbImgOn.style.width = "45rem"
+        lightBulbImgOn.style.height = "60rem"
+        lightBulbImgOn.style.paddingTop = "6rem"
     } else {
+        h1.textContent = "Turn the light on"
+        h1.style.color = "black"
+        h1.style.textShadow = "1px 1px 1px rgb(255, 212, 132)"
         lightBulb.appendChild(lightBulbImgOff)
         lightBulb.removeChild(lightBulbImgOn)
+    
     }
 })
 
