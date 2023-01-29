f365e17248ea2bca0871cd5ea9728aa60ff9f862
wishmeheaven <wishmeheaven3@gmail.com>
2023-01-29 23:55:58 +0200

    22.2

--- 22.1_forms/app/app.js
+++ 22.1_forms/app/app.js
@@ -1,60 +1,47 @@
-/*
-Instructions
-Create a webpage that has a form (asks the user for its name,
-age, and email address) and when the user clicks submit, open
-a text window that writes all the information and asks for his
-confirmation. If he clicks on the confirm button, tell him
-“congratulations you successfully sent this form” if he clicks on
-change information, give him the possibility to change the
-information, and ask again for his confirmation.
-*/
+'use strict'
 
-const infoForm = document.querySelector('#info-form')
+console.log('run')
+const form = document.getElementById('form')
+const userName = document.querySelector('#user-name')
+const userAge = document.querySelector('#user-age')
+const userEmail = document.querySelector('#user-email')
 
-infoForm.addEventListener('submit', function(e){
-    // alert('submit')
+
+form.addEventListener('submit', function(e) {
     e.preventDefault()
-    submitForm()
+    let confirm = document.querySelector('.confirm')
+   confirm.classList.remove('visible')
+    let change = document.querySelector('.change')
+    let name = userName.value
+    let age = userAge.value
+    let email = userEmail.value
+    let text = document.querySelector('.details')
 
-});
+    text.style.flexDirection = 'column'
+    text.textContent = `Name: ${name}, Age: ${age}, Email: ${email}`
+    const confirmButton = document.querySelector('#confirm-button')
+    confirmButton.addEventListener('click', function() {
+        const congrats = document.querySelector('#congrats')
+        congrats.classList.remove('visible')
+        confirm.classList.add('visible')
+        change.classList.add('visible')
+        }
+    )
 
-function submitForm(){
-    console.log('submit')
-
-    const name = document.querySelector('#name').value
-    const age = document.querySelector('#age').value
-    const email = document.querySelector('#email').value
-    
-    const confirmBox = document.createElement('div')
-    confirmBox.classList.add('confirm-box')
-    confirmBox.innerHTML = `   
-        <h2>Are you sure you want to send this form?</h2>
-        <p>Name: ${name}</p>
-        <p>Age: ${age}</p>
-        <p>Email: ${email}</p>
-        <button id="confirm">Confirm</button>
-        <button id="change">Change information</button>
-    `
-    confirmBox.style.display = 'block'
-    confirmBox.style.position = 'absolute'
-    confirmBox.style.top = '50%'
-    confirmBox.style.left = '50%'
-    confirmBox.style.transform = 'translate(-50%, -50%)'
-    confirmBox.style.background = '#FFF'
-    confirmBox.style.padding = '20px'
-    confirmBox.style.border = '1px solid black'
-    confirmBox.style.borderRadius = '15px'
-    confirmBox.style.textAlign = 'center'
-
-
-    document.body.appendChild(confirmBox)
-    
-    const confirm = document.querySelector('#confirm')
-    const change = document.querySelector('#change')
-    
-    if(confirm){
-        alert('Congratulations you successfully sent this form')
-    }else if (change){
-        alert('Change information')
+    let changeButton = document.querySelector('#change-button')
+    changeButton.addEventListener('click', function() {
+        userName.value = ''
+        userAge.value = ''
+        userEmail.value = ''
     }
-}
+    )
+    
+})
+
+console.log('run2')
+
+
+
+
+
+
