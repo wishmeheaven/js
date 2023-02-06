5467b470150ebaffb128516d0e765bd3d9e0e106
wishmeheaven <wishmeheaven3@gmail.com>
2023-02-06 23:39:10 +0200

    29.2_refactoring_async-await

--- 29.2_refactor_with_async_await/app/app.js
+++ 29.2_refactor_with_async_await/app/app.js
@@ -10,27 +10,21 @@ Display to the screen the joke title and the joke itself
 
 const jokeTitle = document.querySelector('.joke-title')
 const jokeText = document.querySelector('.joke-text')
-// const jokeBtn = document.querySelector('.joke-btn')
 const jokeBtn = document.createElement('button')
+
 jokeBtn.textContent = 'Click & Laugh'
 
 document.body.appendChild(jokeBtn)
 
-jokeBtn.addEventListener('click', () => {
-    fetch('https://api.jokes.one/jod')
-        .then((response) => response.json())
-        .then((data) => {
-            // console.log(data.contents.jokes[0].joke.title)
-            // console.log(data.contents.jokes[0].joke.text)
-            const jokeTitleText = document.createTextNode(data.contents.jokes[0].joke.title)
-            const jokeTextText = document.createTextNode(data.contents.jokes[0].joke.text)
-            jokeTitle.appendChild(jokeTitleText)
-            jokeText.appendChild(jokeTextText)
-
-        })
-        .catch((error) => console.log(error))
-})
-
-
-
-
+jokeBtn.addEventListener('click', async () => {
+    try {
+        const response = await fetch('https://api.jokes.one/jod');
+        const data = await response.json();
+        const jokeTitleText = document.createTextNode(data.contents.jokes[0].joke.title);
+        const jokeTextText = document.createTextNode(data.contents.jokes[0].joke.text);
+        jokeTitle.appendChild(jokeTitleText);
+        jokeText.appendChild(jokeTextText);
+    } catch (error) {
+        console.log(error);
+    }
+});
