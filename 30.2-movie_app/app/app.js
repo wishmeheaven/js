27ec837f1f15a00f03dddb1527669a109f39e223
wishmeheaven <wishmeheaven3@gmail.com>
2023-02-08 03:34:04 +0200

    head_explode

--- 30.2-movie_app/app/app.js
+++ 30.2-movie_app/app/app.js
@@ -1,33 +1,11 @@
 'use strict'
-/*
-Let's create a small movie app.
-We will be using the OMDB API.
-We need to register for an API key here.
-You will get an email with your unique API key.
-Read the documentation on what you can do with this API.
-To have some influence look here
-We want a user to type a movie name at the input and get
-the data displayed on the screen:
-1. Movie poster
-2. Movie title
-3. Genre
-4. Year
-5. Plot
-6. Director
-7. Actors
-8. Ratings of movies from IMDB, rotten tomatoes, and
-Metacritic. (make sure that particular movie gives you all
-the ratings. It could be, that movie won’t give you all 3
-ratings
-*/
 
-document.getElementById('btn').addEventListener('click',showMovieInfo)
+document.getElementById('btn').addEventListener('click', showMovieInfo)
 
 const userInput = document.getElementById('user-input')
 
-
 window.onload = function () {
-    userInput.focus();
+    userInput.focus()
 }
 
 userInput.addEventListener('keypress', function (event) {
@@ -38,41 +16,51 @@ userInput.addEventListener('keypress', function (event) {
 
 async function showMovieInfo() {
     const url = `http://www.omdbapi.com/?apikey=584740cb&t=${userInput.value}`
-    // const url = "http://www.omdbapi.com/?apikey=584740cb"
     try {
-    const response = await fetch(url)
-    const movieCard = document.getElementById('result')
-        if (movieCard.src === undefined) {
-            const randomEmoji = Math.ceil(Math.random()*26)
+        const response = await fetch(url)
+        const movieCard = document.getElementById('result')
+        const data = await response.json()
+        if (data.Response === "False") {
+            const randomEmoji = Math.ceil(Math.random() * 17)
             movieCard.innerHTML = `
+            <div class="alert" role="alert">
             <h3>
-                Movie not found <span>
-                  <img id="smily" src="./assets/img/smily${randomEmoji}.jpg" style="width: 5rem; height:5rems;"/>
-                </span>
-            </h3>
+             ${randomEmoji === 12 ?
+                    "Find it or I'll shoot.." :
+                    randomEmoji === 15 ?
+                        "Mm-mm! Mm! Mmm!!" :
+                        "Movie not found"}</h3>
+                <img id="smily" src="./assets/img/smily${randomEmoji}.jpg" style="width: ${randomEmoji === 12 ? 6 : 5}rem; height:${randomEmoji === 12 ? 6 : 5}rems;"/>
+            </div>
             `
-    } else {
-            console.log("userInout", userInput.value)
-            console.log("movieCard", movieCard)
-            const data = await response.json()
-        console.log(data)
-        //// style="width:12rem; height:12rem; cursor: pointer;"
-        userInput.value = ''
-        movieCard.innerHTML = `
-                <img id="movieImg" src="${data.Poster}"/> </br>                
-                <p id="movie-title">${data.Title ? data.Title : ""} (${data.Year})</p>
-                <p id="movie-genre">${data.Genre ? data.Genre : ""}</p>
-                <p id="movie-plot">${data.Plot ? data.Plot : ""}</p>
-                <p id="movie-director">${data.Director ? data.Director : ""}</p>
-                <p id="movie-actors">${data.Actors ? data.Actors : ""}</p>
-                <p id="movie-rating">Rating : ${data.imdbRating ? data.imdbRating : ""}</p>
+        } else {
+            console.log(data)
+            console.log(data.Ratings[0].Value)
+            console.log(data.Ratings[1].Value)
+            console.log(data.Ratings[2].Value)
+            console.log(data.Rated)
+            userInput.value = ''
+            movieCard.innerHTML = `
+                <img id="movieImg" src="${data.Poster}"/>             
+                <p id="movie-title">${data.Title} (${data.Year}) <span id="movie-runtime"> ${data.Runtime}</p>
+                <p id="movie-rated" style=${data.Rated === "R" ? "color:red" : "color:white"}>Rated ${data.Rated}</p>    
+                </p>
+           <p id="movie-genre"><span>Genre</span></br> ${data.Genre}</p>
+                <p id="movie-plot"><span>Plot</span></br>${data.Plot}</p>
+                <p id="movie-director"><span>Director</span></br>${data.Director}</p>
+                <p id="movie-actors"><span>Cast</span></br>${data.Actors}</p>
+                <p id="movie-rating"><span>IMDB Rating</span> ${data.Ratings[0].Value}</p>
+                <p id="movie-metascore"><span>Metascore</span> ${data.Ratings[2].Value}</p>
+                <p id="movie-rotten"><span>Rotten Tomatoes</span> ${data.Ratings[1].Value}</p>
+                <p id="movie-language"><span>Language</span></br>${data.Language}</p>
+                <p id="movie-country"><span>Country</span></br>${data.Country}</p>
+
                 `
-        movieCard.querySelector('img').addEventListener('click', function () {
-            window.open(`https://www.imdb.com/title/${data.imdbID}`, '_blank')
-        })
-    }   
-    } catch(err) {
+            movieCard.querySelector('img').addEventListener('click', function () {
+                window.open(`https://www.imdb.com/title/${data.imdbID}`, '_blank')
+            })
+        }
+    } catch (err) {
         console.log(err)
     }
 }
-
